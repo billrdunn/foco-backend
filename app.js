5e034b378f33446d36380c1d65a0e9c64431baac
Bill Dunn <billrdunn@gmail.com>
2022-07-22 13:25:56 +0100

    Edit backend so heroku can get routes.

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ mongoose
   });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
 }
 
 app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	response.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.use(cors());
